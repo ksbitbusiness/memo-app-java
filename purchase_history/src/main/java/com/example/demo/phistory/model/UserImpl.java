f7be84aa27f3d96a15282008fa2bad0e84f4e2f9
kenmiya-win <kenmiya.works@gmail.com>
2021-06-27 15:18:18 +0900

    code bug fixed.

--- purchase_history/src/main/java/com/example/demo/phistory/model/UserImpl.java
+++ purchase_history/src/main/java/com/example/demo/phistory/model/UserImpl.java
@@ -11,65 +11,22 @@ public class UserImpl implements {
 	
 	private EntityManager entityManager;
     
-}
-
-Shell
-package com.transonic.springboot;
-
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MyDataDaoImpl implements MyDataDao {
-	private static final long serialVersionUID = 1L;
-	
-	private EntityManager entityManager;
-	
-	public MyDataDaoImpl() {
+	public UserImpl() {
 		super();
 	}
 	
-	public MyDataDaoImpl(EntityManager manager) {
+	public UserImpl(EntityManager manager) {
 		this();
 		entityManager = manager;
 	}
-	
+
 	@Override
 	public List getAll() {
-		Query query = entityManager.createQuery("from MyData");
+		Query query = entityManager.createQuery("from User");
+
 		@SuppressWarnings("unchecked")
 		List list = query.getResultList();
 		entityManager.close();
-		// TODO Auto-generated method stub
 		return list;
 	}
-}package com.transonic.springboot;
- 
-public class MyDataDaoImpl implements MyDataDao {
-	private static final long serialVersionUID = 1L;
-	
-	private EntityManager entityManager;
-	
-	public MyDataDaoImpl() {
-		super();
-	}
-	
-	public MyDataDaoImpl(EntityManager manager) {
-		this();
-		entityManager = manager;
-	}
-	
-	@Override
-	public List getAll() {
-		Query query = entityManager.createQuery("from MyData");
-		@SuppressWarnings("unchecked")
-		List list = query.getResultList();
-		entityManager.close();
-		// TODO Auto-generated method stub
-		return list;
-	}
-}
+}}
